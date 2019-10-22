05dbfb89a1e0ddc125c231c26a4d5c876a628db4
nmdoir <nmdoir@gmail.com>
2019-10-22 11:14:38 +0200

    prompt noms des joueurs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 import { Game } from "./Game";
 
+var nom_j1 = prompt("Entrez le nom du joueur 1 :");
+var nom_j2 = prompt("Entrez le nom du joueur 2 :");
+
+if (!nom_j1 || nom_j1 === "") {
+    nom_j1 = "Joueur 1"
+}
+
+if (!nom_j2 || nom_j2 === "") {
+    nom_j2 = "Joueur 2"
+}
+
 $(document).ready(function() {
         let game = new Game(10, 10);
         game.createGrid();
