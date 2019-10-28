481e593ed497371f89bcb40c3acf0bb59fd746e8
nmdoir <nmdoir@gmail.com>
2019-10-28 16:54:23 +0100

    generate empty grid

--- js/main.js
+++ js/main.js
@@ -2,36 +2,37 @@ function createGrid() {
     let bloc = document.getElementById('plateau');
     let table = document.createElement('table');
     let tbody = document.createElement('tbody');
+    table.setAttribute("class", "center");
     table.appendChild(tbody);
 
     for (let i = 0; i < 10; i++) {
         let tr = document.createElement('tr');
+        tr.setAttribute("class","tdstyle");
         tbody.appendChild(tr);
-        //console.log("10 lignes"); OK
 
         for (let j = 0; j < 10; j++) {
             let td = document.createElement('td');
+            td.setAttribute("class","tdstyle");
             tr.appendChild(td);
-            //console.log("10 colonnes"); OK
         }
     }
+
     bloc.appendChild(table);
 }
 
 //Demander le nom des joueurs
-var nom_j1 = prompt("Entrez le nom du joueur 1 : ");
-var nom_j2 = prompt("Entrez le nom du joueur 2 : ");
+var name_j1 = prompt("Entrez le nom du joueur 1 : ");
+var name_j2 = prompt("Entrez le nom du joueur 2 : ");
 
-if (!nom_j1 || nom_j1 === "") {
-    nom_j1 = "Joueur 1";
+if (!name_j1 || name_j1 === "") {
+    name_j1 = "Joueur 1";
 }
 
-if (!nom_j2 || nom_j2 === "") {
-    nom_j2 = "Joueur 2";
+if (!name_j2 || name_j2 === "") {
+    name_j2 = "Joueur 2";
 }
 
 //Créer la grille
 $(document).ready(function() {
     createGrid();
-    //console.log OK
 });
