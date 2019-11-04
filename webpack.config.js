64bece73984c9898089457dcfb34e467db0ec665
nmdoir <nmdoir@gmail.com>
2019-11-04 20:47:51 +0100

    last tests babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,8 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["babel-preset-env"]
+                exclude: /node_modules/,
+                loader: "babel-loader"
                     }
                 }
             }
